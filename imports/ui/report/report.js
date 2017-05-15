585f69bbbe06f12f92cd931a015594e690adae01
Adam <adam.ocallaghan@yahoo.ie>
2017-05-15 21:41:29 +0100

    Reports exporting to PDF now.

--- imports/ui/report/report.js
+++ imports/ui/report/report.js
@@ -18,5 +18,57 @@ Template.report.events({
 
         // Client log
         console.log("Running Report for " + searchTerm);
+        var articleTitles = Articles.find({
+            $or: [
+                {person2: searchTerm},
+                {person1: searchTerm},
+            ]
+        }).map(function(i){
+            return i.title;
+        });
+        var articlePolarity = Articles.find({
+            $or: [
+                {person2: searchTerm},
+                {person1: searchTerm},
+            ]
+        }).map(function(i){
+            return i.polarity;
+        });
+        var articleSource = Articles.find({
+            $or: [
+                {person2: searchTerm},
+                {person1: searchTerm},
+            ]
+        }).map(function(i){
+            return i.source;
+        });
+        var articleScore = Articles.find({
+            $or: [
+                {person2: searchTerm},
+                {person1: searchTerm},
+            ]
+        }).map(function(i){
+            return i.score;
+        });
+
+        // Define the pdf-document
+        var docDefinition = {
+            content: [
+                { text: 'Report for Articles Containing: ' + searchTerm, style: 'headline'},
+                { text: Date()},
+                '----------------------------------------------------------------------------------------------------------------------------',
+                articleTitles,
+                '----------------------------------------------------------------------------------------------------------------------------',
+                { text: 'END', style: 'headline'},
+            ],
+            // Style dictionary
+            styles: {
+                headline: { fontSize: 20, bold: true, margin: [0, 0, 0, 25] },
+                article: { fontSize: 14, margin: [0, 0, 0, 5] },
+            }
+        };
+
+        // Start the pdf-generation process
+        pdfMake.createPdf(docDefinition).open();
     },
 });
